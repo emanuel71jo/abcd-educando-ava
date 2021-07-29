177e1c6b9bbe81a4b2f565ffc7be7bbb524d5b96
emanuel71jo <emanuel71jo@gmail.com>
2021-07-29 13:00:09 -0300

    Pages Register e Login

---
+++ frontend/src/components/authentication/login/LoginForm.js
@@ -0,0 +1,107 @@
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+import { Icon } from '@iconify/react';
+// material
+import {
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  InputAdornment,
+  Stack,
+  TextField
+} from '@material-ui/core';
+import { LoadingButton } from '@material-ui/lab';
+import { Form, FormikProvider, useFormik } from 'formik';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email informado é inválido')
+      .required('Email é um campo obrigatório'),
+    password: Yup.string().required('Senha é um campo obrigatório')
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      remember: true
+    },
+    validationSchema: LoginSchema,
+    onSubmit: () => {
+      navigate('/dashboard', { replace: true });
+    }
+  });
+
+  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
+
+  const handleShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Senha"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleShowPassword} edge="end">
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+        </Stack>
+
+        <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+          <FormControlLabel
+            control={<Checkbox {...getFieldProps('remember')} checked={values.remember} />}
+            label="Mater-me conectado"
+          />
+          {/* 
+          <Link component={RouterLink} variant="subtitle2" to="#">
+            Forgot password?
+          </Link> */}
+        </Stack>
+
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={isSubmitting}
+        >
+          Entrar
+        </LoadingButton>
+      </Form>
+    </FormikProvider>
+  );
+}
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,99 @@
+import { Box, Card, Container, Link, Typography } from '@material-ui/core';
+// material
+import { styled } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import { MHidden } from '../components/@material-extend';
+import { RegisterForm } from '../components/authentication/register';
+// components
+import Page from '../components/Page';
+// layouts
+import AuthLayout from '../layouts/AuthLayout';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Page)(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex'
+  }
+}));
+
+const SectionStyle = styled(Card)(({ theme }) => ({
+  width: '100%',
+  maxWidth: 464,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  margin: theme.spacing(2, 0, 2, 2)
+}));
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  display: 'flex',
+  minHeight: '100vh',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  padding: theme.spacing(12, 0)
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Register() {
+  return (
+    <RootStyle title="Register | Minimal-UI">
+      <AuthLayout>
+        Já possui uma conta? &nbsp;
+        <Link underline="none" variant="subtitle2" component={RouterLink} to="/login">
+          Entrar
+        </Link>
+      </AuthLayout>
+
+      <MHidden width="mdDown">
+        <SectionStyle>
+          <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
+            Ambiente Virtual de Aprendizagem
+          </Typography>
+          <img alt="register" src="/static/illustrations/illustration_register.png" />
+        </SectionStyle>
+      </MHidden>
+
+      <Container>
+        <ContentStyle>
+          <Box sx={{ mb: 5 }}>
+            <Typography variant="h4" gutterBottom>
+              Cadastrar
+            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>
+              Informe a baixo seus dados para cadastro
+            </Typography>
+          </Box>
+
+          {/* <AuthSocial /> */}
+
+          <RegisterForm />
+
+          {/* <Typography variant="body2" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
+            By registering, I agree to Minimal&nbsp;
+            <Link underline="always" sx={{ color: 'text.primary' }}>
+              Terms of Service
+            </Link>
+            &nbsp;and&nbsp;
+            <Link underline="always" sx={{ color: 'text.primary' }}>
+              Privacy Policy
+            </Link>
+            .
+          </Typography> */}
+
+          <MHidden width="smUp">
+            <Typography variant="subtitle2" sx={{ mt: 3, textAlign: 'center' }}>
+              Já possui uma conta?&nbsp;
+              <Link to="/login" component={RouterLink}>
+                Entrar
+              </Link>
+            </Typography>
+          </MHidden>
+        </ContentStyle>
+      </Container>
+    </RootStyle>
+  );
+}
